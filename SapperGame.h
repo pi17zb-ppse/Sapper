8d7e47d249ac67a4602fa0a1f7056212db5c787c
Бондаренко Евгений <prizrak140893@gmail.com>
2021-01-26 18:06:23 +0300

    Коммит 3

--- SapperGame.h
+++ SapperGame.h
@@ -7,21 +7,98 @@
 using namespace std;
 using namespace sf;
 
+
+/**
+* Игра сапер
+* 
+* Класс игры сапер
+* 
+* @autohor Бондаренко Евгений
+* @version 1.0.0.
+* @copyright GNU Public License
+* @todo Реализовать все методы
+*/
+
+
 class SapperGame {
 private:
-	int sapperrWidth = 8;
-	int sapperrHeight = 8;
-	int mines = 10;
+/**
+* Ширина игрового поля
+* 
+* @var int  $sapperrWidth
+* 
+* Высота игрового поля
+*
+* @var int  $sapperrHeight
+* 
+* Количество ми
+* 
+* @var int $mines
+* 
+* Позиция в меню
+* 
+* @var int $position
+*/
+	int sapperrWidth;
+	int sapperrHeight;
+	int mines;
 	int position;
 public:
 	SapperGame();
+/**
+* Метод взаимодейсвий окон
+* @param RenderWindow $window
+*/
 	void Rendering(RenderWindow& window);
 private:
+/**
+* Метод вызова окна меню
+* @param RenderWindow $window
+* @return $position
+*/
 	int windowMenu(RenderWindow& window);
+
+/**
+* Метод вызова окна выбора сложности
+* @param RenderWindow $window
+* @return $position
+*/
 	int windowSettings(RenderWindow& window);
+
+/**
+* Метод вызова окна игры
+* @param RenderWindow $window
+* @return $position
+*/
 	int windowGame(RenderWindow& window);
+
+/**
+* Метод обхода по пустым клетка
+* @param int $gridLogic
+* @param int $gridView
+* @param int $X
+* @param int $Y
+*/
 	void traversalRecursion(int**& gridLogic, int**& gridView, int X, int Y);
+
+/**
+* Метод проверки количеств флажков относительно указателя мины
+* @param int $gridLogic
+* @param int $gridView
+* @param int $X
+* @param int $Y
+* @param int $gameOver
+*/
 	void checkMine(int**& gridLogic, int**& gridView, int x, int y, int& gameOver);
+
+/**
+* Метод обхода вокруг указателя мины
+* @param int $gridLogic
+* @param int $gridView
+* @param int $X
+* @param int $Y
+* @param int $gameOver
+*/
 	void openCells(int**& gridLogic, int**& gridView, int x, int y, int& gameOver);
 };
 
